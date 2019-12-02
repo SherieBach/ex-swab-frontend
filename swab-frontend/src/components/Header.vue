e7133c9bef59858705bd1655a7cf3eaade661ef0
SherieBach <Sherie.klappenbach@gmail.com>
2019-12-02 13:07:24 +0700

    added logo, mouse alt and redid background image

--- swab-frontend/src/components/Header.vue
+++ swab-frontend/src/components/Header.vue
@@ -1,11 +1,18 @@
 <template>
     <div id="header">
-        <div class="bgImage"></div>
+        <div class="bgImage">
+            <img src="../assets/images/Bar.png" class="bar-menu">
+            <img src="../assets/images/softwarelse.png" class="logo">
+            <img src="../assets/images/mouse-alt.png" class="mouse-scroll">
+        </div>
         <nav>
-            <router-link to="/">Home</router-link>  ||
+            <router-link to="/">Home</router-link>
+            ||
             <!---<router-link :to="{ name: consults, hash: '#consults_block' }">Consults</router-link> ||---->
-            <router-link to="/career">Career</router-link>  ||
-            <router-link to="/startown">Start up</router-link>  ||
+            <router-link to="/career">Career</router-link>
+            ||
+            <router-link to="/startown">Start up</router-link>
+            ||
             <!---<router-link :to="{ name: contact, hash: '#contacts_block' }">Contact</router-link>--->
         </nav>
     </div>
@@ -18,23 +25,60 @@
 </script>
 <style scoped lang="scss">
     @import "../styles/global";
+
     div#header {
         width: auto;
-        height: 33em;
-        background-size: 100% auto;
+        height: 55em;
 
-        .bgImage {
-            margin: 0;
-            height: 100%;
-            background-size: cover;
-                background: linear-gradient(
-                                180deg,
-                                rgba(2, 0, 36, 0.024247198879551846) 30%,
-                                rgba(0, 0, 9, 0.5704656862745099) 62%,
-                                rgba(0, 0, 0, 0.923406862745098) 92%),
-                url("../assets/images/joel-filipe-_AjqGGafofE-unsplash 2@x2.jpg") no-repeat fixed center;
+        nav {
+
+            display: none;
+
+            @include lg {
+                display: block;
+            }
         }
 
+        .bgImage {
+            position: relative;;
+            background: linear-gradient(
+                            180deg,
+                            rgba(2, 0, 36, 0.024247198879551846) 40%,
+                            rgba(0, 0, 0, 0.8169642857142857) 92%),
+            url("../assets/images/joel-filipe-_AjqGGafofE-unsplash 2@x2.jpg") no-repeat center;
+            background-size: cover;
+            margin: 0;
+            height: 56%;
+
+        }
+
+        .bar-menu {
+            position: absolute;
+            top: 10px;
+            right: 10px;
+            width: 12%;
+            height: auto;
+        }
+
+        .logo {
+            position: absolute;
+            top: 170px;
+            left: 50px;
+            width: 80%;
+            height: auto;
+            mix-blend-mode: luminosity;
+        }
+
+        .mouse-scroll {
+            position: absolute;
+            bottom: 50px;
+            left: 180px;
+            width: 6%;
+            height: auto;
+            mix-blend-mode: luminosity;
+
+
+        }
 
         a {
             font-weight: bold;
@@ -45,5 +89,7 @@
                 color: #42b983;
             }
         }
+
+
     }
 </style>
