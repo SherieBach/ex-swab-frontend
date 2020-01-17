dab6da5c4e829b1ab5d3949401439f3e85786b1e
SherieBach <Sherie.klappenbach@gmail.com>
2020-01-17 14:02:08 +0100

    added a test mouse over transition

--- swab-frontend/src/components/CurriculumLink.vue
+++ swab-frontend/src/components/CurriculumLink.vue
@@ -1,13 +1,19 @@
 <template>
     <div id="cvLink">  <!-- adding a linked component for each CV and rerouting it, searchable URL tweaked by comp-->
-        <h1 id="consultants_block">Consultants</h1>
-        <div v-if="this.getCVContent" class="cvLink__card">
-            <router-link
-                    :to="{ name: 'curriculum', params: { name: this.getCVContent.cv.firstName + this.getCVContent.cv.lastName }}">
-                <div class="cvLink__profile" >
-                    <h3>{{this.getCVContent.cv.firstName + " " + this.getCVContent.cv.lastName}}</h3>
+        <h1 id="consultants_block">Our consultants</h1>
+        <div v-if="this.getCVContent" class="cvLink-card hoverOn" :style="{ width:bgWidth + 'px' }">
+            <router-link :to="{
+            name: 'curriculum',
+            params: { name: this.getCVContent.cv.firstName +
+            this.getCVContent.cv.lastName }
+            }">
+                <div class="cvLink-card--profile ">
+                <div @mouseover="handleHoverIn"
+                     @mouseout="handleHoverOut">
                     <img src="../assets/images/gabriel.jpg" alt="Thumbnail photo of the consultant">
-                    <p v-for="text in this.getCVContent.cv.introText">{{text}}</p>
+                </div>
+                    <h2>{{this.getCVContent.cv.firstName + " " + this.getCVContent.cv.lastName}}</h2>
+                    <span class="cvLink-card--intro" v-for="text in this.getCVContent.cv.introText">{{text}}</span>
                 </div>
             </router-link>
         </div>
@@ -19,10 +25,22 @@
 
     export default {
         name: 'CurriculumLink',
+        data() {
+            return {
+                bgWidth: 300,
+            }
+        },
         methods: {
             ...mapActions([
                 'loadSingleCvFromJson',
-            ])
+            ]),
+            handleHoverIn() {
+                this.bgWidth = 500;
+
+            },
+            handleHoverOut() {
+                this.bgWidth = 300;
+            }
         },
         computed: {
             ...mapGetters([
@@ -36,39 +54,53 @@
     }
 </script>
 <style scoped lang="scss">
+    @import "../styles/global";
+
+    #cvLink {
+        @include flex($flexDir: column);
+    }
 
     h1 {
-        color: white;
+        color: $aquaWhite;
         text-align: center;
         padding: 15px;
     }
-    .cvLink__card {
-        display: flex;
-        flex-flow: wrap;
+
+    .cvLink-card {
         margin: 10px;
-        border: solid grey 1px;
         padding: 10px;
-        border-radius: 5px;
-        cursor: pointer;
+        width: 300px;
+        height: 250px;
+        overflow: hidden;
 
         a {
-            color: white;
+            color: $aquaWhite;
             text-decoration: none;
-        }
-
-        .cvLink__profile {
-            align-content: end;
 
 
-            img {
-                align-content: center;
-                width: 200px;
-                height: auto;
-                padding: 20px;
+            &:hover {
+                cursor: pointer;
             }
 
         }
+
     }
 
+    .cvLink-card--profile {
+        align-content: end;
+
+        img {
+            width: 250px;
+            height: auto;
+        }
+        .cvLink-card--intro {
+
+        }
+
+    }
+
+    .hoverOn {
+        transition: all 2.5s;
+    }
 
 </style>
