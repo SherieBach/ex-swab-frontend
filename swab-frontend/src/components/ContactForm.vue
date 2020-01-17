7caa848efc699ca4e82a51fa54340e6072ca5d81
SherieBach <Sherie.klappenbach@gmail.com>
2020-01-17 14:04:08 +0100

    added contact form for future use

---
+++ swab-frontend/src/components/ContactForm.vue
@@ -0,0 +1,19 @@
+<template>
+    <div id="contact-form"> <!--Backend contact form - not in progress atm-->
+    </div>
+</template>
+<script>
+
+    export default {
+        name: 'ContactForm'
+    }
+
+</script>
+<style scoped lang="scss">
+    @import "../styles/global";
+
+    #contact-form {
+
+    }
+
+</style>
